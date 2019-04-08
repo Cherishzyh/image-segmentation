91d67503bfa9e67de236685886efd5590acb2dd3
Suns <salan668@gmail.com>
2019-04-08 15:45:01 +0800

    优化了UNet的训练脚本。

--- u_net/data.py
+++ u_net/data.py
@@ -8,21 +8,19 @@ def GetData(data_folder):
     image_list = []
     label_list = []
 
-    while True:
-        for file in file_list:
-            file_path = os.path.join(data_folder, file)
+    for file in file_list:
+        file_path = os.path.join(data_folder, file)
 
-            # data read
-            h5_file = h5py.File(file_path, 'r')
-            image = np.asarray(h5_file['input_0'], dtype=np.float32)
-            label = np.asarray(h5_file['output_0'], dtype=np.uint8)
-            h5_file.close()
+        # data read
+        h5_file = h5py.File(file_path, 'r')
+        image = np.asarray(h5_file['input_0'], dtype=np.float32)
+        label = np.asarray(h5_file['output_0'], dtype=np.uint8)
+        h5_file.close()
 
-            image_list.append(image)
-            label_list.append(label)
+        image_list.append(image)
+        label_list.append(label)
 
-            if len(image_list) >= len(file_list):
-                return np.asarray(image_list), np.asarray(label_list)
+    return np.asarray(image_list), np.asarray(label_list)
 
 
 def test_GetData():
--- u_net/train_hdf5.py
+++ u_net/train_hdf5.py
@@ -1,22 +1,33 @@
 from u_net import u_net
 from data import GetData
+import os
 
-data_folder = r'H:/data/Input_1_Output_1/training'
+training_data_folder = r'H:/data/Input_1_Output_1/training'
+validation_data_folder = r'H:/data/Input_1_Output_1/validation'
+
 save_path = 'H:/data/SaveModel/training.h5'
+store_folder = r'H:/data/SaveModel'
 
-image_list, label_list = GetData(data_folder)
+training_data, training_label = GetData(training_data_folder)
+validation_data, validation_label = GetData(validation_data_folder)
 
-model = u_net((180, 180, 1))
+#training_data.shape = (number, row, col, channel)
+model = u_net(training_data.shape[1:])
 print(model.summary())
 
+'''Save model'''
+model_yaml = model.to_yaml()
+with open(os.path.join(store_folder, 'model.yaml'), "w") as yaml_file:
+    yaml_file.write(model_yaml)
+
+#callbacks
+# 步长，初始设置为1e-3，如果validation loss 连续20代不下降，则步长变为原来的0.5.
+# EarlyStop，如果validation loss 连续100代不下降，则终止
+# CheckPoint，只保存最优模型的权重（对应Validation Loss最小的情况）
+# callbacks = [xxxx, xxxx, xxxx]
+
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+history = model.fit(x=training_data, y=training_label, epochs=5, batch_size=128, verbose=2,
+                    validation_data=(validation_data, validation_label))
 
-try:
-    model.load_weights(save_path)
-    print("加载模型成功！继续训练模型")
-except:
-    print("加载模型失败！开始训练一个新模型")
-
-history = model.fit(x=image_list, y=label_list, validation_split=0.2, epochs=5, batch_size=128, verbose=2)
-
-model.save_weights(save_path)
+model.save_weights(os.path.join(store_folder, 'weights.h5'))
