806398cc4190e4bcab1ffaca9dd7427c3e38266f
cherishzyh <anewdaycherish@gmail.com>
2019-04-08 19:37:32 +0800

    优化u-net

--- u_net/train_hdf5.py
+++ u_net/train_hdf5.py
@@ -1,5 +1,6 @@
 from u_net import u_net
 from data import GetData
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 import os
 
 training_data_folder = r'H:/data/Input_1_Output_1/training'
@@ -11,7 +12,7 @@ store_folder = r'H:/data/SaveModel'
 training_data, training_label = GetData(training_data_folder)
 validation_data, validation_label = GetData(validation_data_folder)
 
-#training_data.shape = (number, row, col, channel)
+# training_data.shape = (number, row, col, channel)
 model = u_net(training_data.shape[1:])
 print(model.summary())
 
@@ -25,9 +26,15 @@ with open(os.path.join(store_folder, 'model.yaml'), "w") as yaml_file:
 # EarlyStop，如果validation loss 连续100代不下降，则终止
 # CheckPoint，只保存最优模型的权重（对应Validation Loss最小的情况）
 # callbacks = [xxxx, xxxx, xxxx]
+callbacks = [
+    ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=20, mode='min'),
+    EarlyStopping(monitor='val_loss', patience=100, mode='min'),
+    ModelCheckpoint(monitor='val_loss', save_best_only=True, mode='min')
+]
+
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-history = model.fit(x=training_data, y=training_label, epochs=5, batch_size=128, verbose=2,
-                    validation_data=(validation_data, validation_label))
+history = model.fit(x=training_data, y=training_label, epochs=10000, batch_size=32, verbose=1,
+                    validation_data=(validation_data, validation_label), callbacks=callbacks)
 
 model.save_weights(os.path.join(store_folder, 'weights.h5'))
