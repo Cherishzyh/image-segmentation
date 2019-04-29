9cdf71c2fdbab34c269b11b8fb97bdfb9c3591d5
Suns <salan668@gmail.com>
2019-04-29 19:09:13 +0800

    Update the hidden layer script.

--- hiddenlayer.py
+++ hiddenlayer.py
@@ -1,43 +1,50 @@
-from keras import backend, Model
+from keras import backend as K
 from data import GetData
 from saveandload import ReadModel
-from filepath import test_data_folder, model_path, best_weights_path
+# from filepath import test_data_folder, model_path, best_weights_path
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 
+K.set_image_data_format('channels_last')
+
+test_data_folder = r'v:\ProstateSegment\data\Input_1_Ouput_1\testing'
 image_test, label_test = GetData(test_data_folder)
+image_test = image_test[[0], ...]
+print(image_test.shape)
 
+model_path = r'C:\Users\yangs\Desktop\temp_zyh\model.yaml'
+best_weights_path = r'C:\Users\yangs\Desktop\temp_zyh\best_weights.h5'
 model = ReadModel(model_path, best_weights_path)
 
 layer_name = []
 for layer in model.layers:
     layer_name.append(layer.name)
 
-# for i in range(len(model.layers)):
-#     hidden_layer = model.get_layer(index=i)
-#     new_model = Model(inputs=model.input, outputs=hidden_layer.output)
-#     result = new_model.predict(image_test)
-#     print(result.shape)
-#     plt.imshow(result[5, :, :, 0], cmap='gray')
+featur_map_root_folder = r'C:\Users\yangs\Desktop\temp_zyh\feature_map'
+for i, layer in enumerate(model.layers):
+    if i == 0:
+        continue
 
-for i in range(len(model.layers)):
-    picture_path = r'H:/data/SaveModel/model/picture'
+    one_hidder_layer_folder = os.path.join(featur_map_root_folder, layer_name[i])
 
-    picture_path = os.path.join(picture_path, layer_name[i+1])
-    os.mkdir(picture_path)
+    if not os.path.exists(one_hidder_layer_folder):
+        os.mkdir(one_hidder_layer_folder)
 
     # get feature map in i+1 layer
-    get_layer_output = backend.function([model.layers[0].input], [model.layers[i+1].output])
+    get_layer_output = K.function([model.layers[0].input], [model.layers[i].output])
 
     # get the feature map of a picture
-    for k in range(len(get_layer_output([image_test]))):
+    output_layer_list = get_layer_output([image_test])
+    print(len(output_layer_list))
+    output_layer = output_layer_list[0]
 
-        layer_output = get_layer_output([image_test])[k]
+    for k in range(output_layer.shape[-1]):
+        layer_output = output_layer[..., k]
 
-        feature_map_name = str(k)
-        picture_path = os.path.join(picture_path, feature_map_name)
+        one_specific_layer_path = os.path.join(one_hidder_layer_folder, str(k) + '.png')
 
         plt.axis('off')
-        plt.imsave(picture_path, layer_output[0, :, :, 0], format="png", cmap='gray')
+        plt.imsave(one_specific_layer_path, layer_output[0, :, :], format="png", cmap='gray')
 
 
