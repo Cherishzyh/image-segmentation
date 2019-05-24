f52f918f69d9fabdd16079ab167835d703e030f5
Suns <salan668@gmail.com>
2019-05-24 14:15:08 +0800

    Update a script for data augmentation.

---
+++ data_augmentation/script.py
@@ -0,0 +1,45 @@
+'''
+1. 如何做扩增，Augmentation
+2. 如何在训练过程中，做Augmentation
+'''
+
+#1.
+def AugmentScript():
+    from MeDIT.SaveAndLoad import LoadNiiData
+    from MeDIT.Visualization import Imshow3DArray, DrawBoundaryOfBinaryMask
+    from MeDIT.Normalize import Normalize01
+    import numpy as np
+    import time
+
+    t2_image, _, t2 = LoadNiiData(r'C:\Users\yangs\Desktop\TZ roi1\CHA YONG BAO\t2.nii', dtype=np.float32)
+    roi_image, _, roi = LoadNiiData(r'C:\Users\yangs\Desktop\TZ roi1\CHA YONG BAO\t2_roi_hy.nii', dtype=np.uint8)
+
+    # Imshow3DArray(t2, ROI=roi)
+
+    t2_slice = t2[..., 10]
+    roi_slice = roi[..., 10]
+
+    # DrawBoundaryOfBinaryMask(t2_slice, roi_slice)
+    import matplotlib.pyplot as plt
+    # plt.imshow(np.concatenate((Normalize01(t2_slice), Normalize01(roi_slice)), axis=1), cmap='gray')
+    # plt.show()
+
+    from MeDIT.DataAugmentor import DataAugmentor2D, AugmentParametersGenerator
+    param_dict = {'stretch_x': 0.1, 'stretch_y': 0.1, 'shear': 0.1, 'rotate_z_angle': 20, 'horizontal_flip': True}
+
+    augment_generator = AugmentParametersGenerator()
+    augmentor = DataAugmentor2D()
+
+    while True:
+        augment_generator.RandomParameters(param_dict)
+        transform_param = augment_generator.GetRandomParametersDict()
+        print(transform_param)
+
+        augment_t2 = augmentor.Execute(t2_slice, aug_parameter=transform_param, interpolation_method='linear')
+        augment_roi = augmentor.Execute(roi_slice, aug_parameter=transform_param, interpolation_method='linear')
+        plt.imshow(np.concatenate((Normalize01(augment_t2), Normalize01(augment_roi)), axis=1), cmap='gray')
+        plt.show()
+
+AugmentScript()
+
+def train():
--- hiddenlayer.py
+++ hiddenlayer.py
@@ -1,33 +1,55 @@
 from keras import backend as K
 import matplotlib.pyplot as plt
 import os
+K.set_image_data_format('channels_last')
 
+def SaveFeatureMap(model, one_data, feature_map_root_folder):
+    '''
+    对keras建立的模型，通过输入一个输入数据，将该输入在网络中通过正向传播，得到的所有feature map进行保存。
+    :param model: keras 已存的模型，目前已通过基于TensorFlow的测试
+    :param one_data: 4D数据，数据组成为 1 x row x col x channel
+    :param feature_map_root_folder: 用以存储的文件夹名
+    :return: None
 
-def GetFeatureMap(model, image_test, picture_path):
-    K.set_image_data_format('channels_last')
+    Apr-30-2019
+    '''
 
+    if not os.path.exists(feature_map_root_folder):
+        os.mkdir(feature_map_root_folder)
+        print('The folder is created: {}'.format(feature_map_root_folder))
+    if not os.path.isdir(feature_map_root_folder):
+        print('The root folder is not a folder: {}'.format(feature_map_root_folder))
+        return None
+    if one_data.ndim != 4:
+        print('The dimension of the input_data should be 4')
+        return None
+    if one_data.shape[0] != 1:
+        print('The first dimmension (the number of samples) should be 1')
+        return None
+
+    # 获得各个隐含层的名字
     layer_name = []
     for layer in model.layers:
         layer_name.append(layer.name)
 
-    feature_map_root_folder = picture_path
-    for i, layer in enumerate(model.layers):
-        if i == 0:
+    for layer_index, layer in enumerate(model.layers):
+        # 输入层跳过
+        if layer_index == 0:
             continue
 
-        one_hidden_layer_folder = os.path.join(feature_map_root_folder, str(i) + '_' + layer_name[i])
+        one_hidden_layer_folder = os.path.join(feature_map_root_folder, str(layer_index) + '_' + layer_name[layer_index])
 
         if not os.path.exists(one_hidden_layer_folder):
             os.mkdir(one_hidden_layer_folder)
 
         # get feature map in i+1 layer
-        get_layer_output = K.function([model.layers[0].input], [model.layers[i].output])
+        get_layer_output = K.function([model.layers[0].input], [model.layers[layer_index].output])
 
         # get the feature map of a picture
-        output_layer_list = get_layer_output([image_test])
-        print(len(output_layer_list))
+        output_layer_list = get_layer_output([one_data])
         output_layer = output_layer_list[0]
 
+        # 保存该隐含层中的每个channel
         for k in range(output_layer.shape[-1]):
             layer_output = output_layer[..., k]
 
@@ -36,7 +58,6 @@ def GetFeatureMap(model, image_test, picture_path):
             plt.axis('off')
             plt.imsave(one_specific_layer_path, layer_output[0, :, :], format="png", cmap='gray')
 
-    return 0
 
 # from data import GetData
 # from saveandload import ReadModel
